ff7a194501ec9401c68d6f3f40edc632abdf1b1a
Jean Carlos Ortiz <jeancarlosortizperez02@gmail.com>
2025-08-05 15:52:41 -0400

    2 horas de estudio de reactiveForm

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: 'reactive',
+    loadChildren: ()=> import('./reactive/reactive.routes').then( module => module.reactiveRoutes),
+  },
+
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes'),
+  },
+
+  {
+    path: 'country',
+    loadChildren: ()=> import('./country/country.routes').then( module => module.countryRoutes),
+  },
+  {
+    path: '**',
+    redirectTo: 'reactive'
+  }
+];
---
+++ src/app/country/country.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from "@angular/router";
+import { CountryPageComponent } from "./country-page/country-page.component";
+
+
+export const countryRoutes : Routes = [
+  {
+    path: '',
+    component: CountryPageComponent
+  }
+]
